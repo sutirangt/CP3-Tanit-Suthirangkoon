d75d9a6cd014e247ffda6bf8d2ac4082545dbf76
sutirangt <71272995+sutirangt@users.noreply.github.com>
2020-10-13 11:24:03 +0700

    Add files via upload

---
+++ Exercise4_Tanit_S.py
@@ -0,0 +1,9 @@
+fe_score = 79.8
+gb_score = 82.5
+ics_score = 68.75
+cp_score = 70.2
+print("---- Your Score ----")
+print("Foundation English", ":", fe_score)
+print("General Business", ":", gb_score)
+print("Introduction of Computer Systems", ":", ics_score)
+print("Computer Programming", ":", cp_score)
